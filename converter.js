605289f1b96f5c2799bfb08936e1b1b06bdfb80b
Damian <swean@vp.pl>
2024-09-16 16:38:22 +0200

    s

--- converter.js
+++ converter.js
@@ -46,7 +46,9 @@ async function converter(){
 
 btnConvert.addEventListener('click',async ()=> {
     btnConvert.style="display: none"
-    const elementy = ["Lp.", "Numer pytania", "Pytanie", "Odpowiedź A", "Odpowiedź B", "Odpowiedź C", "Poprawna odp", "Media", "Kategorie", "Nazwa media tłumaczenie migowe (PJM) treść płyt", "Pytanie [ENG]", "Odpowiedź A [ENG]", "Odpowiedź B [ENG]", "Odpowiedź C [ENG]", "Pytanie [DE]", "Odpowiedź A [DE]", "Odpowiedź B [DE]", "Odpowiedź C [DE]", "Pytanie [UA]", "Odpowiedź A [UA]", "Odpowiedź B [UA]", "Odpowiedź C [UA]"]
+    const elementy = ["Lp.", "Numer pytania", "Pytanie", "Odpowiedź A", "Odpowiedź B", "Odpowiedź C", "Poprawna odp", "Media", "Kategorie", "Nazwa media tłumaczenie migowe (PJM) treść płyt",  "Nazwa media tłumaczenie migowe (PJM) treść odp A",
+      "Nazwa media tłumaczenie migowe (PJM) treść odp B","Nazwa media tłumaczenie migowe (PJM) treść odp C", "Pytanie [ENG]", "Odpowiedź A [ENG]", "Odpowiedź B [ENG]", "Odpowiedź C [ENG]", "Pytanie [DE]", "Odpowiedź A [DE]", "Odpowiedź B [DE]", "Odpowiedź C [DE]", "Pytanie [UA]", "Odpowiedź A [UA]", "Odpowiedź B [UA]", "Odpowiedź C [UA]"]
+    const elementyTakNie = ["Lp.", "Numer pytania", "Pytanie", "Poprawna odp", "Media", "Kategorie", "Nazwa media tłumaczenie migowe (PJM) treść płyt","Nazwa media tłumaczenie migowe (PJM) treść odp A", "Nazwa media tłumaczenie migowe (PJM) treść odp B","Nazwa media tłumaczenie migowe (PJM) treść odp C" , "Pytanie [ENG]", "Pytanie [DE]", "Pytanie [UA]"]
     let text
     let show
     let original
@@ -59,19 +61,35 @@ btnConvert.addEventListener('click',async ()=> {
         show.forEach(element => {
         numberOfQuaestions += 1
         let p = ""
-            for (let i = 1; i < elementy.length; i++){
-                original = element[elementy[i]]
-                if (original != undefined){
-                if (original.includes('"')) {
-                    console.log('should replace')
-                    modified = original.replace(/"/g, "'")
-                    } else {
-                    modified = original
+        if (element['Poprawna odp'].includes('Tak') || element['Poprawna odp'].includes('Nie')){ 
+          for (let i = 1; i < elementyTakNie.length; i++){
+             original = element[elementyTakNie[i]] 
+            if (original != undefined){
+            if (original.includes('"')) {
+                console.log('should replace')
+                modified = original.replace(/"/g, "'")
+                } else {
+                modified = original
                 }
-                }
-                p += `<p>"${elementy[i]}":"${modified}"${i === (elementy.length-1) ? "" : ','}</p>`
+                p += `<p>"${elementyTakNie[i]}":"${modified}"${i === (elementyTakNie.length-1) ? "" : ','}</p>`
+            } 
+        }
+        text += `<div>"q${numberOfQuaestions}":{${p}},</div>`
+        } else {
+          for (let i = 1; i < elementy.length; i++){
+            original = element[elementy[i]]
+            if (original != undefined){
+            if (original.includes('"')) {
+                console.log('should replace')
+                modified = original.replace(/"/g, "'")
+                } else {
+                modified = original
             }
-            text += `<div>"q${numberOfQuaestions}":{${p}},</div>`
+            }
+            p += `<p>"${elementy[i]}":"${modified}"${i === (elementy.length-1) ? "" : ','}</p>`
+        }
+        text += `<div>"q${numberOfQuaestions}":{${p}},</div>`
+        }
         })
         converted.innerHTML = `{${text}}`
     } else {
